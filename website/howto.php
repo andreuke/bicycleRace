c4789c4fd5346653d4002d5c1ce77bab55d5f08d
Ettore Trainti <ettore.trainiti@gmail.com>
2014-10-26 20:53:11 -0500

    Website Draft

---
+++ website/howto.php
@@ -0,0 +1,35 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.ico">
+
+    <title></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <?php include("navbar.php") ?>
+    <div class="container">
+      <div class="row">
+        <div class="page-header">
+          <h1>How to <small>install the app?</small></h1>
+        </div>
+        <div class="col-md-12">
+        </div>
+      </div>
+    </div>
+
+
+    <?php include("footer.php") ?>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
